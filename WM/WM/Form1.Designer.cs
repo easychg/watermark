29109a79b1ad8c9721690d4766f0ee50d5a9e315
easychg <156375001@qq.com>
2018-03-06 14:16:29 +0800

    v1.0

---
+++ WM/WM/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_statusInfo = new System.Windows.Forms.Label();
+            this.btnExec = new System.Windows.Forms.Button();
+            this.txtMark = new System.Windows.Forms.TextBox();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lb_statusInfo);
+            this.groupBox1.Controls.Add(this.btnExec);
+            this.groupBox1.Controls.Add(this.txtMark);
+            this.groupBox1.Controls.Add(this.txtDir);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(481, 453);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(19, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(437, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "注：本工具加水印会删除原文件，只保存加水印后的文件，请将原文件复制一份！";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Location = new System.Drawing.Point(19, 175);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(439, 272);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "水印图：";
+            this.label2.Click += new System.EventHandler(this.lb_selectMark_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "文件夹：";
+            this.label1.Click += new System.EventHandler(this.lb_selectDir_Click);
+            // 
+            // lb_statusInfo
+            // 
+            this.lb_statusInfo.AutoSize = true;
+            this.lb_statusInfo.Location = new System.Drawing.Point(29, 132);
+            this.lb_statusInfo.Name = "lb_statusInfo";
+            this.lb_statusInfo.Size = new System.Drawing.Size(77, 12);
+            this.lb_statusInfo.TabIndex = 3;
+            this.lb_statusInfo.Text = "状态：未执行";
+            // 
+            // btnExec
+            // 
+            this.btnExec.Location = new System.Drawing.Point(383, 127);
+            this.btnExec.Name = "btnExec";
+            this.btnExec.Size = new System.Drawing.Size(75, 23);
+            this.btnExec.TabIndex = 2;
+            this.btnExec.Text = "执行";
+            this.btnExec.UseVisualStyleBackColor = true;
+            this.btnExec.Click += new System.EventHandler(this.btnExec_Click);
+            // 
+            // txtMark
+            // 
+            this.txtMark.Location = new System.Drawing.Point(76, 86);
+            this.txtMark.Name = "txtMark";
+            this.txtMark.Size = new System.Drawing.Size(382, 21);
+            this.txtMark.TabIndex = 1;
+            this.txtMark.Click += new System.EventHandler(this.lb_selectMark_Click);
+            // 
+            // txtDir
+            // 
+            this.txtDir.Location = new System.Drawing.Point(76, 48);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(382, 21);
+            this.txtDir.TabIndex = 0;
+            this.txtDir.Click += new System.EventHandler(this.lb_selectDir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 477);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "水印";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtMark;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Button btnExec;
+        private System.Windows.Forms.Label lb_statusInfo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WM/WM/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace WM
+{
+    public partial class Form1 : Form
+    {
+        List<string> listExtention = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            Form.CheckForIllegalCrossThreadCalls = false;
+            listExtention.AddRange(new string[] { ".jpg", ".gif", ".png" });
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+            Application.ExitThread();
+        }
+        #region 在新线程中运行函数
+        /// <summary>
+        /// 在新线程中运行函数
+        /// </summary>
+        /// <param name="func">传入 函数名(无参、无返回值)</param>
+        /// <param name="IsBackground">是否为后台线程(后台线程，窗口关闭后就终止线程)</param>
+        public static void ThreadNew(VoidFunction func, bool IsBackground)
+        {
+            Thread th1 = new Thread(new ThreadStart(func));
+            th1.IsBackground = IsBackground;//后台线程，窗口关闭后就终止线程
+            th1.Start();
+        }
+        /// <summary>
+        /// 在新线程中运行函数
+        /// </summary>
+        /// <param name="func">传入 函数名(有一个参数、无返回值)</param>
+        /// <param name="para">object参数</param>
+        /// <param name="IsBackground">是否为后台线程(后台线程，窗口关闭后就终止线程)</param>
+        public static Thread ThreadNew(ParamFunction func, object para, bool IsBackground)
+        {
+            Thread th1 = new Thread(new ParameterizedThreadStart(func));
+            //判断状态
+            //((int)th1.ThreadState &((int)ThreadState.Running | (int)ThreadState.Suspended) ) == 0
+            th1.IsBackground = IsBackground;
+            th1.Start(para);
+            return th1;
+        }
+        /// <summary>
+        /// 允许线程之间进行操作
+        /// </summary>
+        public static void OprateBetweenThread()
+        {
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        /// <summary>
+        /// 无参的、返回值为void的委托，可以用来做参数名
+        /// </summary>
+        public delegate void VoidFunction();
+
+        /// <summary>
+        /// 有一个参数的、返回值为void的委托，可以用来做参数名
+        /// </summary>
+        public delegate void ParamFunction(object para);
+
+
+        #endregion
+
+        private void lb_selectDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                txtDir.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void lb_selectMark_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //设置文件类型
+            ofd.Filter = "图片|*.jpg;*.png;*.gif;*.jpeg;*.bmp";
+
+            //设置默认文件类型显示顺序
+            //ofd.FilterIndex = 1;
+
+            //保存对话框是否记忆上次打开的目录
+            ofd.RestoreDirectory = true;
+
+            //点了保存按钮进入
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+                txtMark.Text = ofd.FileName.ToString();
+                //ConfigFile.Instanse["txtMark"] = txtMark.Text;
+                pictureBox1.Image = Image.FromFile(ofd.FileName.ToString());
+            }
+        }
+        int success = 0; //成功
+        int falure = 0; //失败
+        int total = 0;
+        private void MakeWaterMark()
+        {
+            success = 0;
+            falure = 0;
+            total = 0;
+            string errmsg = "";
+            string markPicPath = txtMark.Text.Trim();
+            string strtxtDir = txtDir.Text.Trim();
+            if (strtxtDir == "" || markPicPath == "")
+            {
+                MessageBox.Show("请选择目录和水印文件！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else if (Directory.Exists(strtxtDir) == false)
+            {
+                MessageBox.Show("文件夹不存在！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else
+            {
+                btnExec.Enabled = false;
+                List<string> PictureList = new List<string>();
+                lb_statusInfo.Text = "状态：正在检索图片…";
+                SearchFile(txtDir.Text.Trim(), ref PictureList);
+                foreach (string s in PictureList)
+                {
+                    try
+                    {
+
+                        MakeWaterPic(s, "", markPicPath, "");
+                        success++;
+                    }
+                    catch (Exception er)
+                    {
+                        falure++;
+                        errmsg += er.Message;
+                    }
+                    total++;
+                    lb_statusInfo.Text = "状态：正在为第" + (total + 1) + "张图片加水印…";
+                }
+                lb_statusInfo.Text = "状态：完成！共" + total + ",成功" + success + ",失败" + falure;
+                btnExec.Enabled = true;
+                if (errmsg != "") MessageBox.Show(errmsg, "执行完成，部分文件出错信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        public void SearchFile(string parentDir, ref List<string> PictureList)
+        {
+            try
+            {
+                string[] subFiles = Directory.GetFiles(parentDir);
+                string[] subDirs = Directory.GetDirectories(parentDir, "*.*", SearchOption.TopDirectoryOnly);
+                PictureList.AddRange(subFiles);
+                foreach (string dir in subDirs)
+                {
+                    SearchFile(dir, ref PictureList);
+                }
+            }
+            catch (Exception ex) { }
+        }
+
+
+        private string MakeWaterPic(string SourcePicPath, string WaterText, string WaterPath, string SaveName)
+        {
+            if (File.Exists(SourcePicPath) == false)
+            {
+                return "-1";//文件不存在
+            }
+
+            string extension = Path.GetExtension(SourcePicPath).ToLower();//后缀
+            if (listExtention.Contains(extension) == false) throw new Exception("不允许的后缀:" + SourcePicPath + "\n");
+            string fileName = "";
+            if (SaveName.Trim() != "") fileName = SaveName;
+            else fileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+
+            //加文字水印
+            System.Drawing.Image image = System.Drawing.Image.FromFile(SourcePicPath, true);
+            int imgwidth = image.Width;
+            int imgheight = image.Height;
+            using (System.Drawing.Bitmap bitmap = new Bitmap(image.Width, image.Height))
+            {
+
+                using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(bitmap))//
+                {
+                    g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                    g.Clear(System.Drawing.Color.Transparent);
+                    g.DrawImage(image, 0, 0, imgwidth, imgheight);//画上原图片
+
+                    image.Dispose();
+                    //g.DrawImage(image, 0, 0, image.Width, image.Height);
+                    if (WaterText != "")
+                    {
+                        Font f = new Font("Verdana", 32);
+                        Brush b = new SolidBrush(Color.Yellow);
+                        g.DrawString(WaterText, f, b, 10, 10);
+                    }
+                    //g.Dispose();
+
+                    //加图片水印
+                    System.Drawing.Image copyImage = System.Drawing.Image.FromFile(WaterPath);
+                    //Rectangle[destRect ] 它指定所绘制图像的位置和大小。将图像进行缩放以适合该矩形。
+                    //Rectangle[srcRect ] 它指定 image 对象中要绘制的部分。
+                    g.DrawImage(copyImage, new Rectangle(imgwidth - copyImage.Width, imgheight - copyImage.Height, copyImage.Width, copyImage.Height), 0, 0, copyImage.Width, copyImage.Height, GraphicsUnit.Pixel);
+                    if (File.Exists(SourcePicPath))
+                    {
+                        File.Delete(SourcePicPath);
+                    }
+                    //保存加水印过后的图片,删除原始图片
+                    // string newPath = fileName + extension;
+                    switch (extension)
+                    {
+                        case ".jpg":
+                            bitmap.Save(SourcePicPath, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+                        case ".gif":
+                            bitmap.Save(SourcePicPath, System.Drawing.Imaging.ImageFormat.Gif);
+                            break;
+                        case ".png":
+                            bitmap.Save(SourcePicPath, System.Drawing.Imaging.ImageFormat.Png);
+                            break;
+                        case ".jpeg":
+                            bitmap.Save(SourcePicPath, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+                        case ".bmp":
+                            bitmap.Save(SourcePicPath, System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+                        default:
+                            throw new Exception("不允许的后缀:" + SourcePicPath);
+                    }
+                    pictureBox1.Image = Image.FromFile(SourcePicPath);
+                }
+            }
+
+            return "1";
+            // Response.Redirect(newPath);
+            //}
+        }
+        
+        private void btnExec_Click(object sender, EventArgs e)
+        {
+            ThreadNew(MakeWaterMark, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //txtDir.Text = ConfigFile.Instanse["txtDir"];
+            //txtMark.Text = ConfigFile.Instanse["txtMark"];
+        }
+    }
+}
